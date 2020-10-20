0f8e550a8b4dc6343a117e5f569b5e0a9d4831d8
idvan007 <idvan007@gmail.com>
2020-10-20 18:35:15 +0300

    Add random comment

--- app/src/main/java/com/example/gm1/MapsActivity.java
+++ app/src/main/java/com/example/gm1/MapsActivity.java
@@ -343,7 +343,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 write_marsh.write(String.valueOf(loc.getLatitude()) +" " +String.valueOf(loc.getLongitude())+" "+"\n");
             }
         }
-        lastLocation = loc;
+        lastLocation = loc; // random comment
         tvDistance.setText(String.valueOf(distance));
         tvVelocity.setText(String.valueOf(loc.getSpeed()));
 
